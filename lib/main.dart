76dd5dea3177c4c955ca003b1a9ad3477330bc3c
NiazSagor <niazsagor@gmail.com>
2025-08-20 13:02:25 +0600

    :camera_flash: feat: Implement custom color scheme and theme
    This commit introduces a custom color scheme (`kColorScheme`) and applies it throughout the application using `ThemeData`.
    
    Key changes:
    - Defined `kColorScheme` using `ColorScheme.fromSeed`.
    - Updated `MaterialApp`'s `theme` to use the new color scheme.
    - Customized `appBarTheme` with specific background and foreground colors.
    - Styled `cardTheme` with a background color and margins.
    - Configured `elevatedButtonTheme` with a background color.
    - Adjusted `textTheme` for `titleLarge` to use a specific color, font weight, and size.
    
    Took 23 minutes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
-  runApp(MaterialApp(home: Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          )
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 16,
+          )
+        )
+      ),
+      home: Expenses(),
+    ),
+  );
 }
