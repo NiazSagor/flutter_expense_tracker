a32878d8c67733c8650b3b0e7ff1de1bec1dfaad
NiazSagor <niazsagor@gmail.com>
2025-08-21 20:03:27 +0600

    :globe_with_meridians: feat: Implement dark mode and enhance UI
    This commit introduces dark mode support and several UI improvements to the application.
    
    Key changes:
    - Added `kDarkColorScheme` for dark mode theming.
    - Configured `darkTheme` in `MaterialApp` with specific styles for `CardTheme` and `ElevatedButtonTheme`.
    - Set `themeMode` to `ThemeMode.system` to follow system settings.
    - Updated `ExpenseItem` to use `Theme.of(context).textTheme.titleLarge` for the expense title and set `crossAxisAlignment` to `CrossAxisAlignment.start`.
    - Enhanced `ExpensesList` to show a background color when an item is dismissed, using `Theme.of(context).colorScheme.error`.
    
    Took 21 minutes

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,27 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardThemeData().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: AppBarTheme().copyWith(
@@ -21,16 +39,17 @@ void main() {
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             backgroundColor: kColorScheme.primaryContainer,
-          )
+          ),
         ),
         textTheme: ThemeData().textTheme.copyWith(
           titleLarge: TextStyle(
             fontWeight: FontWeight.bold,
             color: kColorScheme.onSecondaryContainer,
             fontSize: 16,
-          )
-        )
+          ),
+        ),
       ),
+      themeMode: ThemeMode.system,
       home: Expenses(),
     ),
   );
